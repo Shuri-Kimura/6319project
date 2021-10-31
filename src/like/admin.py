70e78ac94ffd9cc54bed0ec2a3e8e311c21fcecf
あかはな <ryusuke.sym0918@gmail.com>
2021-10-31 15:34:38 +0900

    ddd

--- src/like/admin.py
+++ src/like/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.contrib.admin.decorators import register
+from src.users.models import Cfavos, Tfavos
 # Register your models here.
+
+admin.site,register(Cfavos)
+admin.site,register(Tfavos)
+
