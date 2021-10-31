956e449a3f5683d028dea204bb6d484604c03b18
あかはな <ryusuke.sym0918@gmail.com>
2021-10-31 22:32:08 +0900

    k

--- src/like/views.py
+++ src/like/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
-from django.template import loader
 
-from users.models import Cfavos , Tfavos
+from users.models import Cfavos, Tfavos
 
 def index(request):
     return HttpResponse("Hello, world. 6319project.like")
