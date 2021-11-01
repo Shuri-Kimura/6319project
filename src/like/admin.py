6d67415ccd8a5205962aa46c3ffa55ea1d200058
あかはな <ryusuke.sym0918@gmail.com>
2021-11-02 01:16:19 +0900

    j

--- src/like/admin.py
+++ src/like/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
-from users.models import Cfavos, Tfavos
+from ..users.models import Cfavos, Tfavos
 # Register your models here.
 
 admin.site.register(Cfavos)
--- src/like/views.py
+++ src/like/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
-from users.models import Cfavos, Tfavos
+from ..users.models import Cfavos, Tfavos
 
 def index(request):
     return HttpResponse("Hello, world. 6319project.like")
