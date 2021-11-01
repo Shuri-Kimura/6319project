af9d844e20d1923999609cf04c2c171d86eb4a0b
youki22 <youkisoccer28@icloud.com>
2021-11-02 00:48:18 +0900

    youki

--- src/textpage/views.py
+++ src/textpage/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+#def index(request):
     
-    return HttpResponse("Hello, world. 6319project.textpage")
+    #return HttpResponse("Hello, world. 6319project.textpage")
 # Create your views here.
