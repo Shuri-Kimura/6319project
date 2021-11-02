87c0069578af788b7be72be0c8498448014a1ab2
idejun <6319011@ed.tus.ac.jp>
2021-11-02 23:42:52 +0900

    １回目じゅん

--- src/project/settings.py
+++ src/project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'classpages.apps.ClasspagesConfig',
     'like.apps.LikeConfig',
+    'mypage.apps.MypageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
