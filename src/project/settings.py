1501563e94f24b9bf348bb974701e1bfdffae6b4
Laxus0703 <6319076@ed.tus.ac.jp>
2021-11-02 17:20:14 +0900

    ayumu

--- src/project/settings.py
+++ src/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'classpages.apps.ClasspagesConfig',
     'like.apps.LikeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
