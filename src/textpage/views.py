f2df7e31e286ca1efc0eb60b2c3673fc42fe8694
youki22 <youkisoccer28@icloud.com>
2021-11-06 09:59:40 +0900

    youki

--- src/textpage/views.py
+++ src/textpage/views.py
@@ -3,6 +3,11 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "index.html"
 
+    def get_context_data(self):
+        ctext = super().get_context_data()
+        ctext["textname"] = "久保耀希　取扱い説明書"
+        return ctext
+
 #def index(request):
  # content = {
   #'message': 'こんにちは！Djangoテンプレート！'
