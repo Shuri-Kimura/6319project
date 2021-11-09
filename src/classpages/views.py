3763e948eb0dfea1538957e0bf43d7d8bb5ac495
Laxus0703 <6319076@ed.tus.ac.jp>
2021-11-10 01:52:57 +0900

    ayumu

--- src/classpages/views.py
+++ src/classpages/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from classpages.models import Classes
 
-def index(request):
-    return render(request,'classpages/index.html')
+#def index(request):
+    #return render(request,'classpages/index.html')
+    
+class IndexView(generic.ListView):
+    template_name = 'classpages/index.html'
+    context_object_name = 'classes_list'
+
+    def get_queryset(self):
+        return Classes.objects.filter(class_id__isnull = False)
+
+class Classpage(generic.DetailView):
+    model = Classes
+    template_name = 'classpages/class.html'
