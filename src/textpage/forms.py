7a8d7c480c06ef17105616a023dc7d8da0527abc
youki22 <youkisoccer28@icloud.com>
2021-12-05 15:03:52 +0900

    youki

---
+++ src/textpage/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+from users.models import Tcom
+class TcomForm(forms.ModelForm):
+
+    class Meta():
+        #①モデルクラスを指定
+        model = Tcom
+
+        #②表示するモデルクラスのフィールドを定義
+        fields = "__all__"
+
+        #③表示ラベルを定義
+        labels = {'comments': "コメント"
+        }
+
+
+        
+        """
+        #②表示するモデルクラスのフィールドを定義
+        fields = ('class_id','title','info','category','state','days','image1','image2','image3')
+
+        #③表示ラベルを定義
+        labels = {'class_id':"教科ID",
+                  'title':"商品名",
+                  'info':"商品情報",
+                  'category':"カテゴリ",
+                  'state':"商品状態",
+                  'days':"お届け日数",
+                  'image1':"画像１",
+                  'image2':"画像2",
+                 'image3':"画像3",
+        }
+        """
+    
