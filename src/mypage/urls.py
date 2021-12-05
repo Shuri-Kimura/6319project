718f8609523c52db7a818270c81a2ec77e40d8e9
idejun <6319011@ed.tus.ac.jp>
2021-12-05 18:34:13 +0900

    じゅんいち

--- src/mypage/urls.py
+++ src/mypage/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.MypageView.as_view(), name='mypage'),
     path('<int:pk>/edit/', views.CommentUpdate.as_view(), name='edit'),
+    path('<int:pk>/delete_list/', views.DeleteList.as_view(), name='delete_list'),
+    path('<int:pk>/delete/', views.DeleteText.as_view(), name='delete'),
 ]
