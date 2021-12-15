8d94fe618caa642553dc80fa7a6578e22ad6ca67
ide-jun <6319011@ed.tus.ac.jp>
2021-12-15 13:37:13 +0900

    評価をひとり1つだけにする

--- src/classpages/views.py
+++ src/classpages/views.py
@@ -4,64 +4,91 @@ from django.utils import timezone
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .forms import CcomForm,CevalForm,CreateForm
-from users.models import Classes,Users,Cevals,Ccom
+from .forms import CcomForm, CevalForm, CreateForm
+from users.models import Classes, Users, Cevals, Ccom
 from django.urls import reverse
+
 
 def index(request):
     return render(request, 'users/index.html')
 
-#class IndexView(generic.ListView):
+# class IndexView(generic.ListView):
     template_name = 'classpages/index.html'
     context_object_name = 'classes_list'
 
     def get_queryset(self):
-        return Classes.objects.filter(class_id__isnull = False)
+        return Classes.objects.filter(class_id__isnull=False)
 
-class Classpage(generic.DetailView,generic.edit.ModelFormMixin):
+
+class Classpage(generic.DetailView, generic.edit.ModelFormMixin):
     model = Classes
     template_name = 'classpages/class.html'
     form = CevalForm()
     fields = ()
+
     def get_context_data(self, **kwargs):
         context = super(Classpage, self).get_context_data(**kwargs)
         context.update({
-            'avg': Cevals.objects.values('class_id').annotate(avg_rikai=models.Avg('rikai'),avg_raku=models.Avg('raku')),
+            'avg': Cevals.objects.values('class_id').annotate(avg_rikai=models.Avg('rikai'), avg_raku=models.Avg('raku')),
             'ccom_list': Ccom.objects.order_by('date').reverse().all()[:5],
         })
         return context
+
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
             eform = CevalForm(request.POST, request.FILES)
             cl = Classes.objects.get(class_id=self.kwargs['pk'])
             print(cl.class_id)
-            eform = Cevals(class_id = cl, user_id = request.user, rikai = eform.data.get("rikai"), raku = eform.data.get("raku"))
+            eform = Cevals(class_id=cl, user_id=request.user, rikai=eform.data.get(
+                "rikai"), raku=eform.data.get("raku"))
             eform.save()
             return HttpResponseRedirect(reverse('classpages:class', args=(cl.class_id,)))
         form = CevalForm(**self.get_form_kwargs())
         return render(request, 'classpages:class', {
-            "form":form
+            "form": form
         })
 
-    #def get_cfavo():
+    # def get_cfavo():
         #cfavo_list = Cevals.objects.filter(class_id__exact = Classes.objects.class_id)
 
+
 def addceval(request, pk):
+    cl = Classes.objects.get(class_id=pk)
+    TF = True
+    user_ceval = Cevals.objects.filter(
+        user_id=request.user).filter(class_id=cl)
     if request.method == 'POST':
         print("ここは通っている1")
         cevf = CevalForm(request.POST, request.FILES)
         print(pk)
-        cl = Classes.objects.get(class_id=pk)
-        cevf = Cevals(class_id = cl, user_id = request.user, rikai = cevf.data.get("rikai"),raku = cevf.data.get("raku"))
+        if user_ceval:
+            TF = False
+        if TF:
+            cevf = Cevals(class_id=cl, user_id=request.user,
+                          rikai=cevf.data.get("rikai"), raku=cevf.data.get("raku"))
+        else:
+            user_ceval = Cevals.objects.get(user_id=request.user, class_id=cl)
+            user_ceval.full_clean()
+            cevf = Cevals(ceval_id=user_ceval.ceval_id, class_id=cl, user_id=request.user,
+                          rikai=cevf.data.get("rikai"), raku=cevf.data.get("raku"))
         print(cevf)
         cevf.save()
-        ccom_list = Ccom.objects.order_by('date').reverse().all()
         return HttpResponseRedirect(reverse('classpages:class', args=(cl.class_id,)))
-
-    form = CevalForm()
+    if user_ceval:
+        TF = False
+    if TF:
+        form = CevalForm()
+    else:
+        user_ceval = Cevals.objects.get(user_id=request.user, class_id=cl)
+        initial_data = {
+            'rikai': user_ceval.rikai,
+            'raku': user_ceval.raku
+        }
+        form = CevalForm(initial=initial_data)
     return render(request, 'classpages/add_ceval.html', {
-        "form":form
+        "form": form
     })
+
 
 def addccom(request, pk):
     if request.method == 'POST':
@@ -69,20 +96,22 @@ def addccom(request, pk):
         ccomf = CcomForm(request.POST, request.FILES)
         print(pk)
         cl = Classes.objects.get(class_id=pk)
-        ccomf = Ccom(class_id = cl, user_id = request.user, date = timezone.now(),comments = ccomf.data.get("comments"))
+        ccomf = Ccom(class_id=cl, user_id=request.user,
+                     date=timezone.now(), comments=ccomf.data.get("comments"))
         print(ccomf)
         ccomf.save()
         ccom_list = Ccom.objects.order_by('date').reverse().all()
         return HttpResponseRedirect(reverse('classpages:class', args=(cl.class_id,)))
-        #return render(request, 'classpages/class.html', {
-            #'texts': cl,
-            #'ccom_list':ccom_list,
-            #})
+        # return render(request, 'classpages/class.html', {
+        # 'texts': cl,
+        # 'ccom_list':ccom_list,
+        # })
 
     form = CcomForm()
     return render(request, 'classpages/add_comments.html', {
-        "form":form
+        "form": form
     })
+
 
 def createclass(request):
     template_name = 'classpages/create.html'
@@ -90,7 +119,7 @@ def createclass(request):
     if request.method == 'GET':
         form = CreateForm()
         return render(request, template_name, {
-            "form":form
+            "form": form
         })
     if request.method == 'POST':
         create_form = CreateForm(request.POST)
@@ -100,4 +129,3 @@ def createclass(request):
         else:
             ctx['form'] = create_form
             return render(request, template_name, ctx)
-        
